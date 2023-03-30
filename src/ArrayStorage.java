f0c204084900b9e65aeaedbc53148f3bacd27876
Gulnaz <ggf.faery@gmail.com>
2023-03-30 13:30:00 +0300

    "Вступительное ДЗ v.1"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,54 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int countResume;
 
     void clear() {
+        Arrays.fill(storage, 0, countResume, null);
+        countResume = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume newResume) {
+        storage[countResume] = newResume;
+        countResume++;
     }
 
     Resume get(String uuid) {
+        int index = indexResume(uuid);
+        if (index != -1) {
+            return storage[index];
+        }
         return null;
     }
 
+    int indexResume(String uuid) {
+        for (int i = 0; i < countResume; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
     void delete(String uuid) {
+        int index = indexResume(uuid);
+        storage[index] = null;
+        System.arraycopy(storage, index + 1, storage, index, countResume);
+        countResume--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, countResume);
     }
 
     int size() {
-        return 0;
+        return countResume;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -11,7 +11,7 @@ public class MainArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r;
+        Resume resume;
         while (true) {
             System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
@@ -24,34 +24,27 @@ public class MainArray {
                 uuid = params[1].intern();
             }
             switch (params[0]) {
-                case "list":
+                case "list" -> printAll();
+                case "size" -> System.out.println(ARRAY_STORAGE.size());
+                case "save" -> {
+                    resume = new Resume();
+                    resume.uuid = uuid;
+                    ARRAY_STORAGE.save(resume);
                     printAll();
-                    break;
-                case "size":
-                    System.out.println(ARRAY_STORAGE.size());
-                    break;
-                case "save":
-                    r = new Resume();
-                    r.uuid = uuid;
-                    ARRAY_STORAGE.save(r);
-                    printAll();
-                    break;
-                case "delete":
+                }
+                case "delete" -> {
                     ARRAY_STORAGE.delete(uuid);
                     printAll();
-                    break;
-                case "get":
-                    System.out.println(ARRAY_STORAGE.get(uuid));
-                    break;
-                case "clear":
+                }
+                case "get" -> System.out.println(ARRAY_STORAGE.get(uuid));
+                case "clear" -> {
                     ARRAY_STORAGE.clear();
                     printAll();
-                    break;
-                case "exit":
+                }
+                case "exit" -> {
                     return;
-                default:
-                    System.out.println("Неверная команда.");
-                    break;
+                }
+                default -> System.out.println("Неверная команда.");
             }
         }
     }
