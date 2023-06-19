6b44029636ffd95f7b6bc213578bbc8107900e63
Gulnaz <ggf.faery@gmail.com>
2023-06-19 15:02:47 +0300

    HW_Lesson_10: create DataStreamSerializer v.2

--- src/com/urise/webapp/storage/serializationStrategy/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializationStrategy/DataStreamSerializer.java
@@ -5,61 +5,64 @@ import com.urise.webapp.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements SerializationStrategy {
-
     @Override
     public void writeResume(Resume resume, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
             dos.writeUTF(resume.getUuid());
             dos.writeUTF(resume.getFullName());
             Map<ContactType, String> contacts = resume.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }
+            writeCollection(dos, contacts.entrySet(), element -> {
+                dos.writeUTF(element.getKey().name());
+                dos.writeUTF(element.getValue());
+            });
 
             Map<SectionType, Section> sections = resume.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                SectionType sectionType = entry.getKey();
-                Section section = entry.getValue();
+            writeCollection(dos, sections.entrySet(), element -> {
+                SectionType sectionType = element.getKey();
+                Section section = element.getValue();
                 dos.writeUTF(sectionType.name());
                 switch (sectionType) {
                     case OBJECTIVE, PERSONAL -> dos.writeUTF(((TextSection) section).getText());
                     case ACHIEVEMENT, QUALIFICATIONS -> {
                         List<String> listOfString = ((ListSection) section).getList();
-                        dos.writeInt(listOfString.size());
-                        for (String element : listOfString) {
-                            dos.writeUTF(element);
-                        }
+                        writeCollection(dos, listOfString, dos::writeUTF);
                     }
                     case EDUCATION, EXPERIENCE -> {
                         List<Organization> listOfOrg = ((OrganizationSection) section).getListOrganization();
-                        dos.writeInt(listOfOrg.size());
-                        for (Organization org : listOfOrg) {
-                            dos.writeUTF(org.getOrganizationName());
-                            dos.writeUTF(org.getWebsite());
-                            List<Organization.Period> periods = org.getPeriods();
-                            dos.writeInt(periods.size());
-                            for (Organization.Period period : periods) {
+                        writeCollection(dos, listOfOrg, organization -> {
+                            dos.writeUTF(organization.getOrganizationName());
+                            dos.writeUTF(organization.getWebsite());
+                            List<Organization.Period> periods = organization.getPeriods();
+                            writeCollection(dos, periods, period -> {
                                 writeLocalDate(dos, period.getDateStart());
                                 writeLocalDate(dos, period.getDateEnd());
-
                                 dos.writeUTF(period.getTitle());
                                 if (period.getDescription() != null) {
                                     dos.writeUTF(period.getDescription());
                                 } else {
                                     dos.writeUTF("null");
                                 }
-                            }
-                        }
+                            });
+                        });
                     }
                 }
-            }
+            });
+        }
+    }
+
+    private interface ElementWriter<T> {
+        void write(T element) throws IOException;
+    }
+
+    private <T> void writeCollection(DataOutputStream dos, Collection<T> collection, ElementWriter<T> writer) throws IOException {
+        dos.writeInt(collection.size());
+        for (T element : collection) {
+            writer.write(element);
         }
     }
 
@@ -76,20 +79,16 @@ public class DataStreamSerializer implements SerializationStrategy {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             resume = new Resume(uuid, fullName);
-            int contactSize = dis.readInt();
-            for (int i = 0; i < contactSize; i++) {
+            readCollection(dis, () -> {
                 ContactType contactType = ContactType.valueOf(dis.readUTF());
                 String value = dis.readUTF();
                 resume.addContact(contactType, value);
-                System.out.println(contactType.getTitle() + ": " + value);
-            }
-            int sectionSize = dis.readInt();
-            for (int i = 0; i < sectionSize; i++) {
+            });
+            readCollection(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 Section section = readSection(sectionType, dis);
                 resume.addSection(sectionType, section);
-                System.out.println(sectionType.getTitle() + ": " + section);
-            }
+            });
             return resume;
         }
     }
@@ -120,6 +119,18 @@ public class DataStreamSerializer implements SerializationStrategy {
     @FunctionalInterface
     private interface ElementReader<T> {
         T read() throws IOException;
+    }
+
+    @FunctionalInterface
+    private interface SectionReader {
+        void read() throws IOException;
+    }
+
+    private void readCollection(DataInputStream dis, SectionReader reader) throws IOException {
+        int size = dis.readInt();
+        for (int i = 0; i < size; i++) {
+            reader.read();
+        }
     }
 
     private <T> List<T> readList(DataInputStream dis, ElementReader<T> reader) throws IOException {
